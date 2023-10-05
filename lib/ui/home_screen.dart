466a63a1b243a84454c6368d31577e28f609511d
Pradip Jadav <77873775+Pradipjadav05@users.noreply.github.com>
2023-10-05 16:30:15 +0530

    final committed

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_database/model/user_model.dart';
+import 'package:sqlite_database/utils/database_helper.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DatabaseHelper db = DatabaseHelper();
+  List<UserModel> userData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    db.close();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,11 +32,54 @@ class HomeScreen extends StatelessWidget {
         title: const Text("SQLite database"),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [
-          Text("data"),
-        ],
+      body: ListView.builder(
+        itemCount: userData.length,
+        itemBuilder: (context, index) {
+          UserModel user = userData[index];
+          return ListTile(
+            leading: CircleAvatar(child: Text("${user.id}")),
+            title: Text(user.name),
+            subtitle: Text(user.email),
+            trailing: IconButton(
+              onPressed: () async {
+                int res = await db.delete(user.id ?? 0);
+                getData();
+                debugPrint("Record deleted $res");
+              },
+              icon: const Icon(Icons.delete),
+            ),
+            onLongPress: () async {
+              UserModel users = UserModel(
+                  id: user.id,
+                  name: "Rajni",
+                  email: "jadavrajni0599@gmail.com");
+
+              int res = await db.update(users);
+              getData();
+              debugPrint("Record updated $res");
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          UserModel user =
+              UserModel(name: "Pradip", email: "jadavpradip2002@gmail.com");
+          int res = await db.insert(user);
+          debugPrint("Record inserted $res");
+          getData();
+        },
+        child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void getData() async {
+    List<UserModel> users = await db.getUser();
+
+    // Update the state with the retrieved user data
+    setState(() {
+      userData = users;
+    });
   }
 }
--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqlite_database/model/user_model.dart';
 
 class DatabaseHelper {
   // named constructor
@@ -11,6 +12,9 @@ class DatabaseHelper {
 
   // create instance for access DatabaseHelper member.
   static final DatabaseHelper instance = DatabaseHelper.internal();
+
+
+  factory DatabaseHelper() => instance;
 
   static const _dbName = "MyDatabase.db";
   static const _dbVersion = 1;
@@ -20,10 +24,10 @@ class DatabaseHelper {
   static const columnName = "name";
   static const columnEmail = "email";
 
-  static late Database _database;
+  static Database? _database;
 
   //Getter for access database.
-  Future<Database> get getDatabase async {
+  Future<Database?> get getDatabase async {
     if (_database != null) return _database;
     _database = await _initDatabase();
     return _database;
@@ -40,14 +44,49 @@ class DatabaseHelper {
     await db.execute('''
     CREATE TABLE $tableUser ($columnId INTEGER PRIMARY KEY, 
                              $columnName TEXT NOT NULL,
-                             $columnEmail VARCHAR(25) NOT NULL
+                             $columnEmail VARCHAR(30) NOT NULL
                             )
     ''');
   }
 
-  Future<int> insert(Map<String, dynamic> data) async {
-    Database db = await instance.getDatabase;
+  Future<List<UserModel>> getUser() async {
+    Database? db = await instance.getDatabase;
 
-    return await db.insert(tableUser, data);
+    var res = await db!.query(tableUser);
+
+    // Convert the list of maps to a list of UserModel objects
+    List<UserModel> userList = res.map((userMap) {
+      return UserModel(
+        id: userMap[columnId] as int,
+        name: userMap[columnName] as String,
+        email: userMap[columnEmail] as String,
+      );
+    }).toList();
+    return userList;
+  }
+
+  Future<int> insert(UserModel user) async {
+    Database? db = await instance.getDatabase;
+
+    return await db!.insert(tableUser, user.toJson());
+  }
+
+  Future<int> update(UserModel user) async {
+    Database? db = await instance.getDatabase;
+
+    return await db!.update(tableUser, user.toJson(),
+        where: "$columnId=?", whereArgs: [user.id]);
+  }
+
+  Future<int> delete(int id) async {
+    Database? db = await instance.getDatabase;
+    // await db!.execute("DELETE FROM $tableUser WHERE $columnId = $id");
+    // return await db!.delete(tableUser, where: "$columnId=?", whereArgs: [id]);
+    return await db!.rawDelete("DELETE FROM $tableUser WHERE $columnId = $id");
+  }
+
+  Future close() async {
+    Database? db = await instance.getDatabase;
+    return db!.close();
   }
 }
